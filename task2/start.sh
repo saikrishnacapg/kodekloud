7577b4d795188395821a49c672511ee8dfe5cce6
Sai Krishna <saikrishna.capg@gmail.com>
2022-06-09 22:51:28 +0000

    Added Supervisor.d to run multiple process

--- task2/start.sh
+++ task2/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-/usr/local/bin/startup.sh &
 service apparmor start
-tail -f /dev/null
+apparmor_parser -r -W /etc/apparmor.d/bin.ping
+apparmor_parser -r -W /etc/apparmor.d/aa-disable-write
+docker run -it -d --security-opt apparmor:aa-disable-write alpine sh
+docker run -it -d --security-opt apparmor:ping debian:jessie bash
